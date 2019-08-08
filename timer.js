b4578113f2dde9d30292dc67eb8f42cb4a012649
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-08-08 07:46:22 +0000

    Automatically backed up by Learn

--- timer.js
+++ timer.js
@@ -5,19 +5,23 @@ var input = process.argv.shift()
 if (input.includes("s")) {
     var inputNumber = parseInt(input.substring(0,input.indexOf("s")));
     var interval = global.setInterval(function(){
-    if (inputNumber === 0) {
-      global.clearInterval(interval)
-      process.exit()
-    }
-    console.log("Left: "+(--inputNumber)+"s")
+      if (inputNumber === 0) {
+        global.clearInterval(interval)
+        process.exit()
+      }
+      console.log("Left: "+(--inputNumber)+"s")
     },1000)
 } else {
   var inputNumber = parseInt(input.substring(0,input.indexOf("m")));
   var interval = global.setInterval(function(){
-  if (inputNumber === 0) {
-    global.clearInterval(interval)
-    process.exit()
-  }
-  console.log("Left: "+(--inputNumber)+"s")
+    if (inputNumber === 0) {
+      global.clearInterval(interval)
+      process.exit()
+    }
+    if (inputNumber <= 60) {
+      console.log("Left: "+(--inputNumber)+"s")
+    } else {
+      console.log("Left: "+(--inputNumber)+"s")
+    }
   },1000)
 }
