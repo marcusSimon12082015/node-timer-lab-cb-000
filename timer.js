aff22615c4f4aa98c5a8b37df313d59a1f374c23
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-08-08 08:39:45 +0000

    Done.

---
+++ timer.js
@@ -0,0 +1,30 @@
+process.argv.shift()
+process.argv.shift()
+
+var input = process.argv.shift()
+if (input.includes("s")) {
+    var inputNumber = parseInt(input.substring(0,input.indexOf("s")));
+    var interval = global.setInterval(function(){
+      if (inputNumber === 0) {
+        global.clearInterval(interval)
+        process.exit()
+      }
+      console.log("Left: "+(--inputNumber)+"s")
+    },1000)
+} else {
+  var inputNumber = parseInt(input.substring(0,input.indexOf("m")));
+  var numberOfSeconds = inputNumber * 60;
+  var interval = global.setInterval(function(){
+    if (numberOfSeconds === 0) {
+      global.clearInterval(interval)
+      process.exit()
+    }
+    numberOfSeconds--;
+    if (numberOfSeconds < 60) {
+      console.log("Left: "+(numberOfSeconds)+"s")
+    } else {
+      console.log("Left: "+Math.floor(numberOfSeconds/60)+"min "+
+      	(numberOfSeconds%60)+"s")
+    }
+  },1000)
+}
